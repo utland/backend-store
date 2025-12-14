d218c76b1b4f6e5715920a713b18fdea7def434d
utland <sizonenkoilya0@gmail.com>
2025-12-14 22:51:42 +0200

    correct test running

--- test/config/test.config.ts
+++ test/config/test.config.ts
@@ -8,7 +8,7 @@ export const testConfig: ConfigType = {
     database: {
         type: process.env.DATABASE_TYPE,
         host: process.env.DATABASE_TEST_HOST,
-        port: process.env.DATABASE_TEST_PORT,
+        port: process.env.DATABASE_PORT,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_TEST_DB,
